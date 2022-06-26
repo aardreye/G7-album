eeb5cdcc18c7b4f728f2237134733af28903a48a
aardreye <aardreye@espol.edu.ec>
2022-06-25 23:22:48 -0500

    first commit

---
+++ scripts/ejercicio.js
@@ -0,0 +1,31 @@
+/*const ejecutarCodigo = () => {
+    alert('Hola, mundo')
+};
+
+ejecutarCodigo();*/
+/*window.onload = inicio;*/
+/*const titulo1 = document.getElementById('titulo1');
+titulo1.textContent = 'Album de fotos';*/
+
+const texto = document.getElementsByClassName('alb');
+texto  .textContent = 'ya funciona!!!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
